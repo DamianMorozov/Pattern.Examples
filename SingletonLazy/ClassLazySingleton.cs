24069b7c32ba615f06bd8f1ddcefb6b79d5069d6
Damian <DamianMorozov@users.noreply.github.com>
2018-08-20 10:55:49 +0300

    Add SingletonLazy - Example of Lazy Singleton.

---
+++ SingletonLazy/ClassLazySingleton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SingletonLazy
+{
+    public sealed class ClassLazySingleton
+    {
+        private static readonly Lazy<ClassLazySingleton> _instance = new Lazy<ClassLazySingleton>(() => new ClassLazySingleton());
+
+        public static ClassLazySingleton Instance => _instance.Value;
+
+#pragma warning disable CS0628 // The new protected member is declared in a sealed class
+        private ClassLazySingleton() { }
+#pragma warning restore CS0628 // The new protected member is declared in a sealed class
+
+        public static void SomeStaticMethod()
+        {
+            Console.WriteLine(@"call public static void SomeStaticMethod()");
+        }
+    }
+}
---
+++ SingletonLazy/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SingletonLazy
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine(@"----------------------------------------------------------------------");
+            Console.WriteLine(@"---            Examples of program development patterns            ---");
+            Console.WriteLine(@"---                         Lazy Singleton                         ---");
+            Console.WriteLine(@"----------------------------------------------------------------------");
+
+            Console.WriteLine(@"ClassLazySingleton.Instance = " + ClassLazySingleton.Instance);
+            Console.WriteLine(@"ClassLazySingleton.Instance.GetHashCode() = " + ClassLazySingleton.Instance.GetHashCode());
+            ClassLazySingleton.SomeStaticMethod();
+            Console.WriteLine(@"----------------------------------------------------------------------");
+
+            Console.WriteLine(@"Press Enter to close.");
+            Console.ReadLine();
+        }
+    }
+}
